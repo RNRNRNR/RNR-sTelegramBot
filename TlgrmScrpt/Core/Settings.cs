3c3f7cd06a7e3113636e4a067ea25c7776441275
RNR <guzhsasha@gmail.com>
2018-11-16 17:02:46 +0200

    no message

---
+++ TlgrmScrpt/Core/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TlgrmScrpt.Core
+{
+    class Settings
+    {
+        private string Apihash { get; set; }
+        private int Apiid { get; set; }
+
+    }
+}
